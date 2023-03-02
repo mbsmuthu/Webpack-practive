e3e0ef1ec881bf81676de14055b7977b879ffca6
muthu <muthu9192@gmail.com>
2023-03-02 19:31:30 +0530

    argsfdvc

--- Webpack-practice.js
+++ Webpack-practice.js
@@ -1,6 +1,11 @@
 const Changebgcolor = () => {
     console.log("Hello");
+<<<<<<< HEAD
     console.log("1234")
     console.log("Hello")
     console.log("Muthu")
+=======
+    console.log("Muthu");
+    console.log("fsdfasdfas");
+>>>>>>> 6884714 (Commit masdfasz)
 };
